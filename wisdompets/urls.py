704d4f640312214e741aa31fa21401fcc29c1d03
hcote <hcote12@gmail.com>
2018-08-17 18:17:48 -0700

    added css and js

--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
     path('adoptions/<int:id>', views.pet_detail, name='pet_detail'),
+    path('', views.home, name='home'),
 ]
